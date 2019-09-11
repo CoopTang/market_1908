3710c01b2ea11f53991588fbb4cf210524a6080c
Michael Cooper <macoop2363@gmail.com>
2019-09-11 14:10:05 -0600

    Added additional tests and their fucntionality

--- lib/market.rb
+++ lib/market.rb
@@ -17,4 +17,21 @@ class Market
   def vendors_that_sell(item)
     @vendors.find_all { |vendor| vendor.inventory[item] > 0 }
   end
+
+  def sorted_item_list
+    @vendors
+      .collect { |vendor| vendor.inventory.keys }
+      .flatten
+      .uniq
+      .sort
+  end
+
+  def total_inventory
+    stock = {}
+    inventories = @vendors.collect { |vendor| vendor.inventory }
+    inventories.each do |inventory|
+      stock.merge!(inventory) { |_, old_val, new_val| old_val + new_val }
+    end
+    stock
+  end
 end
